02dec8e8dbee291c6616b88e8b8d177aeceb5534
S1fTred <matvei.tchufistov@gmail.com>
2025-10-11 17:37:47 +0300

    CharacterController

--- src/main/java/org/sft/tabletoprpg/api/CharacterController.java
+++ src/main/java/org/sft/tabletoprpg/api/CharacterController.java
@@ -1,4 +1,27 @@
 package org.sft.tabletoprpg.api;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sft.tabletoprpg.service.CharacterService;
+import org.sft.tabletoprpg.service.dto.character.CharacterDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/api/characters")
+@RequiredArgsConstructor
 public class CharacterController {
+
+    private final CharacterService characterService;
+
+    @PostMapping("/{campaignId}/characters")
+    public ResponseEntity<CharacterDto> createCharacter(){
+
+    }
+
 }
