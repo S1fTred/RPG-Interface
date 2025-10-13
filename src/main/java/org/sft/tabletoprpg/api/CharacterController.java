26265f6631a8f5c2cbb98e1f49c2383e2fccfe9d
S1fTred <matvei.tchufistov@gmail.com>
2025-10-13 12:59:17 +0300

    CharacterController и ItemController (продолж)

--- src/main/java/org/sft/tabletoprpg/api/CharacterController.java
+++ src/main/java/org/sft/tabletoprpg/api/CharacterController.java
@@ -1,14 +1,20 @@
 package org.sft.tabletoprpg.api;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.sft.tabletoprpg.service.CharacterService;
+import org.sft.tabletoprpg.service.dto.character.CharacterCreateRequest;
 import org.sft.tabletoprpg.service.dto.character.CharacterDto;
+import org.sft.tabletoprpg.service.exception.BadRequestException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @Validated
@@ -19,8 +25,36 @@ public class CharacterController {
 
     private final CharacterService characterService;
 
-    @PostMapping("/{campaignId}/characters")
-    public ResponseEntity<CharacterDto> createCharacter(){
+    @PostMapping("/create/{campaignId}")
+    public ResponseEntity<CharacterDto> createCharacter(
+        @PathVariable UUID campaignId,
+        @AuthenticationPrincipal(expression = "id") UUID requesterId,
+        @Valid @RequestBody CharacterCreateRequest req,
+        UriComponentsBuilder uriBuilder
+    ){
+        if (!campaignId.equals(req.campaignId())){
+            throw new BadRequestException("Кампании не соответствуют");
+        }
+
+
+    }
+
+    @GetMapping("/by-campaign-id/{campaignId}")
+    public List<CharacterDto> listByCampaign(@PathVariable UUID campaignId){
+
+    }
+
+    @GetMapping("/by-owner-id/{ownerId}")
+    public List<CharacterDto> listByOwner(@PathVariable UUID ownerId){
+
+    }
+
+    @PatchMapping("/set-hp/{characterId}")
+    public CharacterDto setHp(
+        @PathVariable UUID characterId,
+        @AuthenticationPrincipal(expression = "id") UUID requesterId,
+        @RequestParam("hp") int
+    ){
 
     }
 
--- src/main/java/org/sft/tabletoprpg/api/ItemController.java
+++ src/main/java/org/sft/tabletoprpg/api/ItemController.java
@@ -1,4 +1,66 @@
 package org.sft.tabletoprpg.api;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sft.tabletoprpg.repo.ItemRepository;
+import org.sft.tabletoprpg.service.dto.item.ItemCreateRequest;
+import org.sft.tabletoprpg.service.dto.item.ItemDto;
+import org.sft.tabletoprpg.service.dto.item.ItemUpdateRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/api/items")
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    @PostMapping("/create")
+    public ResponseEntity<ItemDto> createItem(
+        @AuthenticationPrincipal(expression = "id") UUID requesterId,
+        @Valid @RequestBody ItemCreateRequest req,
+        UriComponentsBuilder uriBuilder
+    ){
+
+    }
+
+    @PatchMapping("/update/{itemId}")
+    public ResponseEntity<Void> updateItem(
+        @PathVariable UUID itemId,
+        @AuthenticationPrincipal(expression = "id") UUID requesterId,
+        @Valid @RequestBody ItemUpdateRequest req
+    ){
+
+    }
+
+    @DeleteMapping("/delete/{itemId}")
+    public ResponseEntity<Void> deleteItem(
+        @PathVariable UUID itemId,
+        @AuthenticationPrincipal(expression = "id") UUID requesterId
+    ){
+
+    }
+
+    @GetMapping("/get-item/{itemId}")
+    public ItemDto getItemById(@PathVariable UUID itemId){
+
+    }
+
+    @GetMapping
+    public List<ItemDto> findItemsByNameContains(
+        @RequestParam(name = "query", required = false) String query
+    ){
+
+    }
+
 }
